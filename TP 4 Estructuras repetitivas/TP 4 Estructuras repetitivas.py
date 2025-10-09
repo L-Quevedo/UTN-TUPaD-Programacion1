32adc0d776640c4c41676389f11bc8234ca6f367
L-Quevedo <l.m.quevedo@hotmail.com>
2025-10-09 19:41:37 -0300

    TP 5 listas completo

--- TP 4 Estructuras repetitivas/TP 4 Estructuras repetitivas.py
+++ TP 4 Estructuras repetitivas/TP 4 Estructuras repetitivas.py
@@ -89,9 +89,32 @@ print(f"La suma de los números entre 0 y {numero} es: {suma}")
 # negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
 # menor, pero debe estar preparado para procesar 100 números con un solo cambio).
 
+cantidad_numeros = 100
 
+pares = 0
+impares = 0
+positivos = 0
+negativos = 0
 
+print(f"Ingresa {cantidad_numeros} números enteros:")
 
+for i in range(cantidad_numeros):
+    numero = int(input(f"Ingresa el número {i + 1}: "))
+    if numero > 0:
+        positivos += 1
+    elif numero < 0:
+        negativos += 1
+
+    if numero % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+
+print(f"\nResultados:")
+print(f"Cantidad de números pares: {pares}")
+print(f"Cantidad de números impares: {impares}")
+print(f"Cantidad de números positivos: {positivos}")
+print(f"Cantidad de números negativos: {negativos}")
 
 
 #9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
