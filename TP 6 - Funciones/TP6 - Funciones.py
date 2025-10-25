a5515e2868a8793c3552b5ad821370fa32252951
L-Quevedo <l.m.quevedo@hotmail.com>
2025-10-25 20:54:20 -0300

    TP6 funciones realizado

---
+++ TP 6 - Funciones/TP6 - Funciones.py
@@ -0,0 +1,42 @@
+#TP6 - Funciones - Quevedo Lucas
+
+
+
+#1. Crear una función llamada imprimir_hola_mundo que imprima por
+# pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+# programa principal.
+
+def imprimir_hola_mundo():
+    print("Hola mundo!")
+
+imprimir_hola_mundo()
+
+
+#2. Crear una función llamada saludar_usuario(nombre) que reciba
+# como parámetro un nombre y devuelva un saludo personalizado.
+# Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa
+# principal solicitando el nombre al usuario.
+
+def saludar_usuario(nombre):
+    return f"Hola {nombre}!"
+
+nombre = input("Ingrese su nombre: ")
+print(saludar_usuario(nombre))
+
+
+#3. Crear una función llamada informacion_personal(nombre, apellido,
+# edad, residencia) que reciba cuatro parámetros e imprima: “Soy
+# [nombre] [apellido], tengo [edad] años y vivo en [residencia]”. Pedir los datos al usuario y llamar a esta función con los valores ingresados.
+
+def informacion_personal (nombre, apellido, edad, residencia):
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+residencia = input("Ingrese su pais de residencia: ")
+
+informacion_personal(nombre, apellido, edad, residencia)
+
+
+#4. 
