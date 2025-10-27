fc60525e55c03df6755dc8545deabfdf7766042e
L-Quevedo <l.m.quevedo@hotmail.com>
2025-10-27 16:40:15 -0300

    pruebas sobre el TP integrador

--- TP Integrador/TPI - Gestión de datos de países.py
+++ TP Integrador/TPI - Gestión de datos de países.py
@@ -1,13 +1,9 @@
 #TPI - Gestión de datos de países - Quevedo Lucas, Aranburu Juan
 
 
-import csv
-from pathlib import Path
-
-#Ruta del archivo
-ruta_csv = Path("Dataset_lista_paises.csv")
-
-print("Verificando archivo…")
-print("Ruta actual:", Path().absolute())
-print("Existe el archivo CSV:", ruta_csv.exists())
+with open("Dataset_lista_paises.csv","r") as archivo:
+    for linea in archivo:
+        linea = linea.strip()
+        partes = linea.split(",")
+        print(partes)
 
